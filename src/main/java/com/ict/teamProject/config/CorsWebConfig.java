b112883633cc7ee4d83bab70e8f79c8d2c4ef76e
tjdus <dusml0277@gmail.com>
2025-10-14 13:43:02 +0900

    fix

--- src/main/java/com/ict/teamProject/config/CorsWebConfig.java
+++ src/main/java/com/ict/teamProject/config/CorsWebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration("globalWebConfig")
-public class WebConfig {
+public class CorsWebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
